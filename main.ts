738ae741fbb43384c48cd3742a7576d82e00d9a3
JaimeDragons22 <101519803+JaimeDragons22@users.noreply.github.com>
2022-03-14 21:23:35 +0100

    Actualizar pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,18 @@
 namespace StatusBarKind {
     export const Points = StatusBarKind.create()
 }
-/**
- * La parte 2 del juego es para 3 de abril de 2023. El juego es para CodeFighters.
- */
+// La parte 2 del juego es para 3 de abril de 2023. El juego es para CodeFighters.
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    game.splash("!Un rayo nooooooooooo¡")
-    game.splash("Que mal en serio te volvere a ver en la parte 2.!Adios¡")
-    game.splash("Si deseas probar la nueva version espera al 3 de abril de 2023")
+    mySprite3 = sprites.create(assets.image`Rayo`, SpriteKind.Player)
+    game.splash("!Genial tu puntuacion es 40¡ ")
+    game.splash("!Si que asi podras llegar 100 puntos¡")
     game.over(true)
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     game.splash("Rendirse")
     game.over(false)
 })
+let mySprite3: Sprite = null
 scene.setBackgroundImage(assets.image`AssetScreen`)
 game.setDialogFrame(assets.image`Text`)
 game.splash("Space Invaders")
